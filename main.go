00afc397a82afb7fbd84f4fc5379370161a3ac14
evan <evanchopra7698@gmail.com>
2020-01-01 18:36:23 -0800

    Added some GPIO stuff and organization

--- main.go
+++ main.go
@@ -7,17 +7,50 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
+
+	rpio "github.com/stianeikeland/go-rpio"
 )
 
-func main() {
+type Machine struct {
+	RoomNum     string
+	MachineType string
+	Status      int
+}
 
-	roomNum := os.Getenv("RoomNum")
-	machineType := os.Getenv("MachineType")
+func intializePins() rpio.Pin {
+	err := rpio.Open()
+	if err != nil {
+		os.Exit(1)
+	}
+	defer rpio.Close()
+	pin := rpio.Pin(19)
+	pin.Mode(rpio.Pwm)
+	pin.Freq(9000)
+	pin.Input()
+	return pin
+	//Need to verify that this works with sensors that I have
+	//Need to verify the freq rate is also correct
 
+}
+
+func doEvery(d time.Duration, f func(time.Time)) {
+	for x := range time.Tick(d) {
+		f(x)
+	}
+}
+
+func readSensorData(pin rpio.Pin) int {
+	res := pin.Read()
+	//Add logic in once I see what the output usually is.
+	//Update status in Struct
+}
+
+func sendRequest(m Machine) {
 	requestBody, err := json.Marshal(map[string]interface{}{
-		"roomnum":     roomNum,
-		"machinetype": machineType,
-		"status":      -1,
+		"roomnum":     m.RoomNum,
+		"machinetype": m.MachineType,
+		"status":      m.Status,
 	})
 	if err != nil {
 		fmt.Println(err)
@@ -35,4 +68,19 @@ func main() {
 	fmt.Println(string(body))
 
 	defer resp.Body.Close()
+
+}
+func main() {
+
+	roomNum := os.Getenv("RoomNum")
+	machineType := os.Getenv("MachineType")
+	var m Machine
+	m.RoomNum = roomNum
+	m.MachineType = machineType
+	pin := intializePins()
+	for {
+		time.Sleep(5 * time.Second)
+		x := readSensorData(pin)
+		sendRequest(m)
+	}
 }
